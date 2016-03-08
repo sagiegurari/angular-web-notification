41489b9e5e26804e783bbba6a333e47657df062a
sagiegurari <sagiegurari@gmail.com>
2016-03-08 23:51:06 +0200

    jsdoc

--- angular-web-notification.js
+++ angular-web-notification.js
@@ -198,7 +198,7 @@
          *    onClick: function onNotificationClicked() {
          *      console.log('Notification clicked.');
          *    },
-         *    autoClose: 4000 //auto close the notification after 2 seconds (you can manually close it via hide function)
+         *    autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
          * }, function onShow(error, hide) {
          *    if (error) {
          *        window.alert('Unable to show notification: ' + error.message);
