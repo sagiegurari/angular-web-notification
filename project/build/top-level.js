1fd20f8a3914ff43b9fe8ee85023b1860e26af63
sagiegurari <sagiegurari@gmail.com>
2016-04-08 13:18:57 +0300

    grunt upgrade

--- project/build/top-level.js
+++ project/build/top-level.js
@@ -1,15 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-    grunt.registerTask('lint', 'Linting tasks.', [
-        'jsonlint:full',
-        'jshint:full',
-        'jslint:full',
-        'eslint:full',
-        'jscs:full',
-        'todos:full'
-    ]);
-
     grunt.registerTask('coverage-prepare', 'Pre test tasks', [
         'cleanup'
     ]);
