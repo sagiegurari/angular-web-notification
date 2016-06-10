eb40e729eb147dcd7a4cea3e885c22d57fe17691
sagiegurari <sagiegurari@gmail.com>
2016-06-10 17:43:47 +0300

    markdown lint changes

--- project/build/markdownlint.js
+++ project/build/markdownlint.js
@@ -1,25 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-    var markdownlint = require('markdownlint');
-
-    /*eslint-disable no-invalid-this*/
-    grunt.registerMultiTask('markdownlint', function task() {
-        var done = this.async();
-
-        markdownlint({
-            files: this.filesSrc,
-            config: this.data.config
-        }, function callback(error, result) {
-            var resultString = error || ((result || '').toString());
-            if (resultString) {
-                grunt.fail.warn('\n' + resultString + '\n');
-            }
-            done(!error || !resultString);
-        });
-    });
-    /*eslint-enable no-invalid-this*/
-
     return {
         tasks: {
             markdownlint: {
