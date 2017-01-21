604ce274dc55460faeca78ac391a6a6001981cf5
sagiegurari <sagiegurari@gmail.com>
2017-01-22 01:11:41 +0200

    docs

--- angular-web-notification.js
+++ angular-web-notification.js
@@ -23,41 +23,48 @@
      * @returns {Object} The service instance
      *
      * @description
-     * The web notification service wraps the HTML 5 Web Notifications API as an angular service.<br>
-     * See [simple-web-notification](https://github.com/sagiegurari/simple-web-notification/blob/master/docs/api.md) for detailed API.
-     *
-     * @example
-     * ```js
-     * angular.module('exampleApp').directive('showButton', ['webNotification', function (webNotification) {
-     *   return {
-     *     link: function (scope, element) {
-     *     element.on('click', function onClick() {
-     *       webNotification.showNotification('Example Notification', {
-     *         body: 'Notification Text...',
-     *         icon: 'my-icon.ico',
-     *         onClick: function onNotificationClicked() {
-     *           console.log('Notification clicked.');
-     *         },
-     *         autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
-     *         }, function onShow(error, hide) {
-     *           if (error) {
-     *             window.alert('Unable to show notification: ' + error.message);
-     *           } else {
-     *             console.log('Notification Shown.');
-     *
-     *             setTimeout(function hideNotification() {
-     *               console.log('Hiding notification....');
-     *               hide(); //manually close the notification (you can skip this if you use the autoClose option)
-     *             }, 5000);
-     *           }
-     *         });
-     *       });
-     *     }
-     *   };
-     * }]);
-     * ```
+     * The web notification service wraps the HTML 5 Web Notifications API as an angular service.
      */
     webNotification.factory('webNotification', function onCreateService() {
+        /**
+         * Shows the notification based on the provided input.<br>
+         * The callback invoked will get an error object (in case of an error, null in
+         * case of no errors) and a 'hide' function which can be used to hide the notification.
+         *
+         * @function
+         * @memberof! webNotification
+         * @alias webNotification.showNotification
+         * @public
+         * @param {String} [title] - The notification title text (defaulted to empty string if null is provided)
+         * @param {Object} [options] - Holds the notification data (web notification API spec for more info)
+         * @param {String} [options.icon=/favicon.ico] - The notification icon (defaults to the website favicon.ico)
+         * @param {Number} [options.autoClose] - Auto closes the notification after the provided amount of millies (0 or undefined for no auto close)
+         * @param {function} [options.onClick] - An optional onclick event handler
+         * @param {ShowNotificationCallback} [callback] - Called after the show is handled.
+         * @example
+         * ```js
+         * webNotification.showNotification('Example Notification', {
+         *   body: 'Notification Text...',
+         *   icon: 'my-icon.ico',
+         *   onClick: function onNotificationClicked() {
+         *     console.log('Notification clicked.');
+         *   },
+         *   autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
+         * }, function onShow(error, hide) {
+         *   if (error) {
+         *     window.alert('Unable to show notification: ' + error.message);
+         *   } else {
+         *     console.log('Notification Shown.');
+         *
+         *     setTimeout(function hideNotification() {
+         *       console.log('Hiding notification....');
+         *       hide(); //manually close the notification (you can skip this if you use the autoClose option)
+         *     }, 5000);
+         *   }
+         * });
+         * ```
+         */
+
         return webNotificationAPI;
     });
 }(window.webNotification));
--- docs/api.md
+++ docs/api.md
@@ -1,40 +1,9 @@
 <a name="webNotification"></a>
 
 ## webNotification ⇒ <code>Object</code>
-The web notification service wraps the HTML 5 Web Notifications API as an angular service.<br>
-See [simple-web-notification](https://github.com/sagiegurari/simple-web-notification/blob/master/docs/api.md) for detailed API.
+The web notification service wraps the HTML 5 Web Notifications API as an angular service.
 
 **Kind**: global namespace  
 **Returns**: <code>Object</code> - The service instance  
 **Ngdoc**: service  
 **Author:** Sagie Gur-Ari  
-**Example**  
-```js
-angular.module('exampleApp').directive('showButton', ['webNotification', function (webNotification) {
-  return {
-    link: function (scope, element) {
-    element.on('click', function onClick() {
-      webNotification.showNotification('Example Notification', {
-        body: 'Notification Text...',
-        icon: 'my-icon.ico',
-        onClick: function onNotificationClicked() {
-          console.log('Notification clicked.');
-        },
-        autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
-        }, function onShow(error, hide) {
-          if (error) {
-            window.alert('Unable to show notification: ' + error.message);
-          } else {
-            console.log('Notification Shown.');
-
-            setTimeout(function hideNotification() {
-              console.log('Hiding notification....');
-              hide(); //manually close the notification (you can skip this if you use the autoClose option)
-            }, 5000);
-          }
-        });
-      });
-    }
-  };
-}]);
-```
